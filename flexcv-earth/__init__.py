11506fdb16706c69eef81f3b4c50db51536a432d
radlfabs <rosenthal.fabian@gmail.com>
2023-11-15 17:17:11 +0100

    migrate earth modules from radlfabs/flexcv

---
+++ flexcv-earth/__init__.py
@@ -0,0 +1,4 @@
+"""This module implements a wrapper class for the Earth Regressor from R. `rpy2` is used to call the R functions from Python.
+
+Author: Fabian Rosenthal
+"""
---
+++ flexcv-earth/model_postprocessing.py
@@ -0,0 +1,63 @@
+import logging
+from pprint import pformat
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+from neptune.types import File
+
+from flexcv.model_postprocessing import ModelPostProcessor
+
+logger = logging.getLogger(__name__)
+mpl.use("Agg")
+style_talk = "seaborn-talk"  # refer to plt.style.available
+
+
+class EarthModelPostProcessor(ModelPostProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, results_all_folds, fold_result, features, run, *args, **kwargs):
+        """Postprocessing function for the MARS model.
+        Logs the parameters to Neptune.
+        Creates a variable importance table and logs barplots to neptune.
+
+        Args:
+            results_all_folds: A dict of results for all folds
+            fold_result: A dataclass containing the results for the current fold
+            run: neptune run object
+            *args: any additional arguments
+            **kwargs: any additional keyword arguments
+
+        Returns:
+            (dict): updated results dictionary
+        """
+        with plt.style.context("ggplot"):
+            imp_df: pd.DataFrame = fold_result.best_model.get_variable_importance(
+                kwargs["features"]
+            )
+            run["MARS/FeatImportance/Table"].append(File.as_html(imp_df))
+            for col in imp_df.columns:
+                # plot all rows of col where col is not 0
+                fig = plt.figure()
+                tmp = imp_df[col]
+                tmp = tmp[tmp != 0]
+                try:
+                    tmp.plot.barh()
+                    plt.title(f"{col} Variable Importance")
+                    run[f"MARS/FeatImportance/"].append(fig)
+                except Exception as e:
+                    logger.info(f"{e}")
+                    logger.info("Could not plot MARS barplot. Continuing.")
+                del fig
+                plt.close()
+
+            run[f"{fold_result.model_name}/BestParams"].append(
+                pformat(fold_result.best_params)
+            )
+
+        return results_all_folds
+
+
+if __name__ == "__main__":
+    pass
