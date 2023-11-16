73de4b645e3c60f90e903912f8c0e9db8c242afb
radlfabs <rosenthal.fabian@gmail.com>
2023-11-16 18:23:31 +0100

    add init with copyright move files

--- flexcv-earth/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This module implements a wrapper class for the Earth Regressor from R. `rpy2` is used to call the R functions from Python.
-
-Author: Fabian Rosenthal
-"""
-
-from .models import EarthRegressor
-from .model_postprocessing import EarthModelPostProcessor
